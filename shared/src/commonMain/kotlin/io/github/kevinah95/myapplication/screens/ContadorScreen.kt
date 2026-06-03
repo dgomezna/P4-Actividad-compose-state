ba9748076d534afb660ea8a7cd25ac6a75b0c553
Daniel Gomez <dgnavarro06@gmail.com>
2026-06-02 20:37:16 -0600

    paso-2: uso remember para preservar el estado

--- shared/src/commonMain/kotlin/io/github/kevinah95/myapplication/screens/ContadorScreen.kt
+++ shared/src/commonMain/kotlin/io/github/kevinah95/myapplication/screens/ContadorScreen.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,7 +23,7 @@ import androidx.compose.ui.unit.dp
 //             ¿Por qué ocurre esto? Agrega un comentario explicando el problema.
 @Composable
 fun ContadorScreen() {
-    var count = 0  // No funciona: Al ser una variable "comun" osea var se reinicia cada vez que se
+    var count by remember { mutableStateOf(0) } // No funciona: Al ser una variable "comun" osea var se reinicia cada vez que se
     // recomponen los componentes, por lo tanto el contador no incrementa, siempre se muestra 0.
 
     Column(
